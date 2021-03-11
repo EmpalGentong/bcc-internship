e72a245309f7d76fb46879cfa4b6d80932e109ae
Ubuntu <ubuntu@ip-172-31-52-29.ec2.internal>
2021-03-11 16:47:57 +0000

    set up online server

--- models/index.js
+++ models/index.js
@@ -1,17 +1,9 @@
 const{ Sequelize } = require('sequelize');
 const env = process.env;
 
-const sequelize = new Sequelize(env.DB_Name, env.DB_User, '', {
-    host: env.DB_Host,
-    dialect: 'mysql',
-    operatorsAliases: false,
-
-    pool: {
-        max: 3,
-        min: 0,
-        acquire: env.DB_ACQUIRE_POOL,
-        idle: env.DB_IDLE_POOL   
-    }
+const sequelize = new Sequelize('freelaunch','user', 'freelaunch123', {
+    host: 'localhost',
+    dialect: 'mysql'
 });
 
 // const table
