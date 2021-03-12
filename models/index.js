c8a0a381e5d6ea8559b10fa3cb10b2b768009bec
EmpalGentong <saihan50@gmail.com>
2021-03-12 20:24:29 +0700

    typo fix & clear view ejs

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const{ Sequelize } = require('sequelize');
 const env = process.env;
 
-const sequelize = new Sequelize(env.DB_Name,env.DB_User, '', {
+const sequelize = new Sequelize(env.DB_Name,env.DB_User, env.DB_User, {
     host: env.DB_Host,
     dialect: 'mysql'
 });
