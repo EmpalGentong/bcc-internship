cd29c9b6b695be2a7561cfcea67bda433d45190d
EmpalGentong <saihan50@gmail.com>
2021-03-16 09:59:47 +0700

    flaggy

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static("yourDesign"))
+app.use(express.static("views"))
 app.set('view-engine','ejs')
+app.set('views', './views');
 
 const userRoutes = require('./routes/user.routes');
 const designRoutes = require('./routes/design.route');
@@ -23,6 +25,9 @@ app.get('/',(req,res)=>{
     })
 })
 
+app.get('/bendera',(req,res,next)=>{
+    res.send('MZWGCZ33ONSWYNDNMF2F6ZDBORQU4Z27MRUV64DPOJXXG7I=')
+})
 
 app.use('/user', userRoutes)
 app.use('/design',designRoutes)
